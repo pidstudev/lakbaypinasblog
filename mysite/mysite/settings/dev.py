66180a4b1b597feb7effba61736761e65aa37c40
pidstudev <pidstudev@gmail.com>
2023-12-21 14:14:14 +0800

    Initialize Wagtail Setup

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-jg@8pcenn3py8u)b%31kd#q=*nbdw_#6_nb*+!@fk5xs0x35on"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
