30ad6186e37bcb5a883cc1e9691ffe97aeecc3e8
pidstudev <pidstudev@gmail.com>
2023-12-22 15:12:12 +0800

    Set up header and site-menu

--- mysite/base/templatetags/navigation_tags.py
+++ mysite/base/templatetags/navigation_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from base.models import FooterText
 
+from wagtail.models import Site
+
 
 register = template.Library()
 
@@ -17,3 +19,8 @@ def get_footer_text(context):
     return {
         "footer_text": footer_text,
     }
+
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    return Site.find_for_request(context["request"]).root_page
